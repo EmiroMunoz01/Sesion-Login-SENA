37632d0be416f0e27d7665c69aa07c64c630a6db
EmiroMunoz01 <emiro_2001@icloud.com>
2023-11-16 15:49:12 -0500

    Se establecieron variables de entorno

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 
+//convertiremos los valores enviados por el body en un formato entendible para el backend
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
@@ -10,10 +11,10 @@ dotenv.config({ path: "./env/.env" });
 app.use("/resources", express.static("public"));
 app.use("/resources", express.static(__dirname + "/public"));
 
+//establecemos el motor de plantillas
 app.set("view engine", "ejs");
 
-//invoraremos al modulo de hashing para encriptar las contrasenas
-
+//
 const bcryptjs = require("bcryptjs");
 
 //esto es un middleware que almacena los datos de sesion en el servidor
@@ -28,9 +29,21 @@ app.use(
 
 const conexion = require("./database/db");
 
+
+//aqui estan las rutas de la pagina
+
 app.get("/", (req, res) => {
-  res.send("Hola mundo");
+  res.render("index",{msg: 'Esto es un mensaje desde NodeJS'});
 });
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
 
 app.listen(3000, (req, res) => {
   console.log("Server Runing in http://localhost:3000");
